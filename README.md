dc24376c452a7852a4a1d921127e615d60dc5c0d
Xiangyang Ju <xju@lbl.gov>
2025-04-08 23:00:04 -0700

    check inputs and outputs

--- README.md
+++ README.md
@@ -7,22 +7,13 @@ To validate the installation of Triton Client (C++ version)
 Use the model from [Triton server tutorial with PyTorch](https://github.com/triton-inference-server/tutorials/tree/main/Quick_Deploy/PyTorch).
 
 ### Usage
-Start the server:
+
+#### Start the server:
 ```bash
 ./scripts/start_triton_server.sh
 ```
 
-Start the client
-```bash
-TRITON_IMAGE="docker.io/docexoty/tritonserver:latest" && podman-hpc run -it --gpu --rm --ipc=host --net=host \
-  --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}:/workspace/ -w /workspace $TRITON_IMAGE bash
-
-pip install torchvision==0.18.0
-
-python models/resnet50/client.py
-```
-
-### Build the client
+#### Build the client and run the test
 ```bash
 cmake -B build -S src -DCMAKE_INSTALL_PREFIX=/pscratch/sd/x/xju/athena_dev/triton_20250408_nosystemmd/install
 
@@ -33,10 +24,6 @@ cmake --build build -- install
 The expected output from the above command is:
 ```text
 [xju@login38] validate_triton_client >cmake --build  build -- install && ./build/bin/test_resnet50  -u "login16:8001" -i models/resnet50/img1.txt 
--- Found RE2 via CMake.
--- Configuring done (0.4s)
--- Generating done (0.1s)
--- Build files have been written to: /pscratch/sd/x/xju/code/validate_triton_client/build
 [ 50%] Building CXX object CMakeFiles/test_resnet50.dir/test_resnet50.cxx.o
 [100%] Linking CXX executable bin/test_resnet50
 [100%] Built target test_resnet50
@@ -46,8 +33,20 @@ Install the project...
 -- Set non-toolchain portion of runtime path of "/pscratch/sd/x/xju/athena_dev/triton_20250408_nosystemmd/install/bin/test_resnet50" to ""
 Reading input file: models/resnet50/img1.txt
 total input entries: 150528
-Input: [1.015926 1.033051 1.015926 1.015926 1.015926 ]
-Expected Input: [1.015926  1.0330508 1.015926  1.015926  1.015926 ]
-Output: [-0.257271 4.352922 -2.057417 -1.998921 -3.177508 ]
-Expected: [-0.25864124  4.3533406  -2.0574622  -1.9991533  -3.178356]
+Input values: [1.015926 1.033051 1.015926 1.015926 1.015926 ]
+Expected input: [1.015926 1.033051 1.015926 1.015926 1.015926]
+Input values match expected values within 0.01
+Output values: [-0.257271 4.352922 -2.057417 -1.998921 -3.177508 ]
+Expected output: [-0.258641 4.353341 -2.057462 -1.999153 -3.178356]
+Output values match expected values within 0.01
+```
+
+Start the client
+```bash
+TRITON_IMAGE="docker.io/docexoty/tritonserver:latest" && podman-hpc run -it --gpu --rm --ipc=host --net=host \
+  --ulimit memlock=-1 --ulimit stack=67108864 -v ${PWD}:/workspace/ -w /workspace $TRITON_IMAGE bash
+
+pip install torchvision==0.18.0
+
+python models/resnet50/client.py
 ```
--- src/test_resnet50.cxx
+++ src/test_resnet50.cxx
@@ -33,8 +33,9 @@ int main(int argc, char* argv[])
     std::string input_file = "img1.bin";
     std::string url("localhost:8001");
     std::string model_name("resnet50");
+    float tol = 0.01;
     
-    while ((opt = getopt(argc, argv, "vhu:i:")) != -1) {
+    while ((opt = getopt(argc, argv, "vhu:i:t:")) != -1) {
         switch (opt) {
             case 'v':
                 verbose = true;
@@ -44,6 +45,9 @@ int main(int argc, char* argv[])
                 break;
             case 'i':
                 input_file = optarg;
+                break;
+            case 't':
+                tol = std::stof(optarg);
                 break;
             case 'h':
                 help = true;
@@ -71,35 +75,59 @@ int main(int argc, char* argv[])
     std::cout << "total input entries: " << inputValues.size() << std::endl;
     std::vector<int64_t> inputShape{3, 224, 224};
 
-    // print the first 5 values of input
-    std::string input_values = "[";
-    for (size_t i = 0; i < 5; ++i) {
-        input_values += std::to_string(inputValues[i]);
-        if (i != inputValues.size() - 1) {
-            input_values += " ";
+    // veryfy input values
+    std::vector<float> expectedInputValues{
+        1.015926, 1.0330508, 1.015926, 1.015926, 1.015926
+    };
+    auto vec_to_string = [](const std::vector<float>& vec, size_t max_size = 5) -> std::string {
+        if (vec.size() < max_size) {
+            return "[Wrong size]";
         }
+        std::string result = "[";
+        for (size_t i = 0; i < max_size; ++i) {
+            result += std::to_string(vec[i]);
+            if (i != vec.size() - 1) {
+                result += " ";
+            }
+        }
+        result += "]";
+        return result;
+    };
+    auto all_close = [](const std::vector<float>& a, const std::vector<float>& b, float tolerance) -> bool {
+        for (size_t i = 0; i < b.size(); ++i) {
+            if (std::abs(a[i] - b[i]) > tolerance) {
+                return false;
+            }
+        }
+        return true;
+    };
+    std::cout << "Input values: " << vec_to_string(inputValues) << std::endl;
+    std::cout << "Expected input: " << vec_to_string(expectedInputValues) << std::endl;
+    if (!all_close(inputValues, expectedInputValues, tol)) {
+        std::cerr << "Input values do not match expected values within " << tol << std::endl;
+        return -1;
+    } else {
+        std::cout << "Input values match expected values within " << tol << std::endl;
     }
-    input_values += "]";
-    std::cout << "Input: " << input_values << std::endl;
-    std::string expected_input_values = "[1.015926  1.0330508 1.015926  1.015926  1.015926 ]";
-    std::cout << "Expected Input: " << expected_input_values << std::endl;
 
     m_client->AddInput<float>("input__0", inputShape, inputValues);
     std::vector<float> outProbs;
     std::vector<int64_t> outShape{1, 1000};
     m_client->GetOutput<float>("output__0", outProbs, outShape);
 
-    std::string expected_values = "[-0.25864124  4.3533406  -2.0574622  -1.9991533  -3.178356]";
-    std::string output_values = "[";
-    for (size_t i = 0; i < 5; ++i) {
-        output_values += std::to_string(outProbs[i]);
-        if (i != outProbs.size() - 1) {
-            output_values += " ";
-        }
+    std::vector<float> expectedOutputValues{
+        -0.25864124, 4.3533406, -2.0574622, -1.9991533, -3.178356
+    };
+
+    std::cout << "Output values: " << vec_to_string(outProbs) << std::endl;
+    std::cout << "Expected output: " << vec_to_string(expectedOutputValues) << std::endl;
+    if (!all_close(outProbs, expectedOutputValues, tol)) {
+        std::cerr << "Output values do not match expected values within " << tol << std::endl;
+        return -1;
+    } else {
+        std::cout << "Output values match expected values within " << tol << std::endl;
     }
-    output_values += "]";
-    std::cout << "Output: " << output_values << std::endl;
-    std::cout << "Expected: " << expected_values << std::endl;
+
     return 0;
 }
 
